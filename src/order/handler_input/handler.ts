3b13b4427a22537fd53d0ee6565f0b889b722625
Danilo Bustos <maximiliano.mendoza@acamica.com>
2020-08-24 11:55:59 -0300

    basic configurations

---
+++ src/order/handler_input/handler.ts
@@ -0,0 +1,47 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { SQS } from 'aws-sdk';
+
+const sqs = new SQS();
+
+const inputOrder: APIGatewayProxyHandler = async (event, context) => {
+  let statusCode: number = 200;
+  let message: string;
+
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'No body was found',
+      }),
+    };
+  }
+  
+  const region = context.invokedFunctionArn.split(':')[3];
+  const accountId = context.invokedFunctionArn.split(':')[4];
+  const queueName: string = 'OrderQueue';
+  const queueUrl: string = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`
+  
+
+  try {
+    await sqs.sendMessage({
+      QueueUrl: queueUrl,
+      MessageBody: event.body,
+    }).promise();
+
+    message = 'order add to the Queue!';
+
+  } catch (error) {
+    console.log(error);
+    message = error;
+    statusCode = 500;
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify({
+      message,
+    }),
+  };
+};
+
+export default inputOrder;
