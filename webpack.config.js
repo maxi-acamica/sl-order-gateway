084d2fd6a1ea5439ee4172be28de105267cd18cd
Maximiliano Mendoza <mmendoza@aivo.co>
2020-08-26 12:05:53 -0300

    using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 
 module.exports = {
-  mode: slsw.lib.webpack.isLocal ? 'dev' : 'prod',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
   devtool: 'source-map',
   resolve: {
