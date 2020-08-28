7a735b5217ecd88192218ce1274154bfd843b546
Maximiliano Mendoza <mmendoza@aivo.co>
2020-08-28 20:32:39 -0300

    log prop

--- helpers/updateSamTemplate.js
+++ helpers/updateSamTemplate.js
@@ -13,11 +13,14 @@ try {
     let fileContents = fs.readFileSync('templates/sam-template.yml', 'utf8');
     let data = yaml.safeLoad(fileContents);
     console.log(data);
+    
     // update json object
     if (data.Resources[0] !== undefined)
     {
         data.Resources[0].Properties.CodeUri = "./dist/queue_order.zip";
+        console.log(data.Resources[0].Properties);
     } else {
+        console.log(data.Resources.Properties);
         data.Resources.Properties.CodeUri = "./dist/queue_order.zip";
     }
     // convert to string object
