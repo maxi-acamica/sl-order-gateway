ceb5827d7d2016dfd2431905a318c9728595684d
Maximiliano Mendoza <mmendoza@aivo.co>
2020-08-29 09:43:19 -0300

    fix real path zip

--- helpers/updateSamTemplate.js
+++ helpers/updateSamTemplate.js
@@ -13,15 +13,17 @@ try {
     let fileContents = fs.readFileSync('templates/sam-template.yml', 'utf8');
     let data = yaml.safeLoad(fileContents);
     console.log(data);
-    
+    // get real path
+    basePath = path.resolve(__dirname + '/../');
+    console.log($basePath);
     // update json object
     if (data.Resources[0] !== undefined)
     {
-        data.Resources[0].SlOrderGatewayStageQueue_order.Properties.CodeUri = "./dist/queue_order.zip";
+        data.Resources[0].SlOrderGatewayStageQueue_order.Properties.CodeUri = basePath+"/dist/queue_order.zip";
         console.log(data.Resources[0].SlOrderGatewayStageQueue_order.Properties);
     } else {
         console.log(data.Resources.SlOrderGatewayStageQueue_order.Properties);
-        data.Resources.SlOrderGatewayStageQueue_order.Properties.CodeUri = "./dist/queue_order.zip";
+        data.Resources.SlOrderGatewayStageQueue_order.Properties.CodeUri = basePath+"/dist/queue_order.zip";
     }
 
     // convert to string object
